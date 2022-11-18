a8168f2aaf29a59eba2ecd0caa131315deb46706
sampadghosh5 <112960743+sampadghosh5@users.noreply.github.com>
2022-11-17 19:33:36 -0500

    Update main.c

--- main.c
+++ main.c
@@ -21,10 +21,21 @@ int main(void)
     //--------------------------------------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "Mancala");
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+<<<<<<< HEAD
+=======
+    
+    Color startColor = RED;
+    Color instructionsColor = RED;
+    
+    int startHeight = screenHeight - (150 + screenHeight / 15);
+    int instructionsHeight = screenHeight - 150;
+    //--------------------------------------------------------------------------------------
+>>>>>>> 149decaf9b2ebf1bd8f5fc87535092244ec81e28
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+<<<<<<< HEAD
         switch (current_window) {
             case 0:
                 main_menu();
@@ -33,6 +44,38 @@ int main(void)
             case 1:
                 choose_game_mode();
                 break;
+=======
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+        
+        
+        if((startHeight -5 < GetMouseY()) 
+            && (GetMouseY() < startHeight+25) 
+            && (35< GetMouseX())
+            && (GetMouseX()< 1000)){
+            
+            startColor = GREEN;
+            if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)){
+                //continue to start menu
+                startColor = BLUE;
+            }
+        }
+        else startColor = RED;
+        if((instructionsHeight -5 < GetMouseY()) 
+            && (GetMouseY() < instructionsHeight+25)
+            && (35< GetMouseX())
+            && (GetMouseX()< 1000)){
+            
+            instructionsColor = GREEN;
+            if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)){
+                //continue to start menu
+                instructionsColor = BLUE;
+            }
+        }
+        else instructionsColor = RED;
+>>>>>>> 149decaf9b2ebf1bd8f5fc87535092244ec81e28
 
             case 9:
                 instructions();
