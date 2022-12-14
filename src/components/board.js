68b7bed83447d958eccaa82a204cd14d6e8cf347
Olivia Brown <99828822+oliviab598@users.noreply.github.com>
2022-12-14 15:55:11 -0500

    Fix

--- src/components/board.js
+++ src/components/board.js
@@ -1,17 +1,21 @@
+let pits;
 pits = Array(14).fill(3);
 pits[0] = 0;
 pits[7] = 0;
 
+let i = 0;
 function updateBoard(index /* index of pit */) {
-    pits_copy = pits.slice();
-    carry = pits_copy[i];
-    pits_copy[i] = 0;
-    i++
-    while(carry > 0) {
-       pits_copy[i] += 1;
-       carry--;
-       i++;
-       if(i == 14)  i = 0;
-    }
-    return pits_copy;
+  let pits_copy;
+  pits_copy = pits.slice();
+  let carry;
+  carry = pits_copy[i];
+  pits_copy[i] = 0;
+  i++;
+  while (carry > 0) {
+    pits_copy[i] += 1;
+    carry--;
+    i++;
+    if (i === 14) i = 0;
+  }
+  return pits_copy;
 }
